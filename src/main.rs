979cd913973b6145288a0a75a346c64030dcdc7c
Goddinger, Philip (SE-A/34) <philip.goddinger@volkswagen.de>
2023-06-04 14:48:04 +0200

    is now faster but still mhmm...

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use lazy_static::lazy_static;
 use std::time::Instant;
-
 use rand::{Rng, seq::SliceRandom};
 
 lazy_static! {
-    static ref WORD_LIST: Vec<char> = generate_word_list();
+    static ref WORD_LIST: Vec<char> = make_char_list();
 }
 
-fn generate_rand_word(number: i32) -> String {
+fn generate_rand_word(number: u8) -> String {
     let mut rng = rand::thread_rng();
     let mut word = String::new();
     for _ in 0..number {
@@ -20,7 +19,7 @@ fn generate_rand_word(number: i32) -> String {
 // generate a word with a random lenght between 2 and 10 from a list of possible characters
 // and returns it as a String
 
-fn generate_rand_word_with_random_length(lower_range: i8, upper_range: i8) -> String {
+fn generate_rand_word_with_random_length(lower_range: u8, upper_range: u8) -> String {
     let mut rng = rand::thread_rng();
     let mut word = String::new();
     let length = rng.gen_range(lower_range..=upper_range);
@@ -31,27 +30,15 @@ fn generate_rand_word_with_random_length(lower_range: i8, upper_range: i8) -> St
     word
 }
 
-// a list containing all the possible characters that can be used to generate a word
-
-fn generate_word_list() -> Vec<char> {
-    let mut word_list = Vec::new();
-    for c in 'a'..='z' {
-        word_list.push(c);
+fn make_char_list() -> Vec<char>{
+    let mut char_list: Vec<char> = Vec::new();
+    for i in 33..=126 {
+        char_list.push(char::from(i));
     }
-    for c in 'A'..='Z' {
-        word_list.push(c);
-    }
-    for c in '0'..='9' {
-        word_list.push(c);
-    }
-    let special_chars = vec![':', ';', '?', '!'];
-    for c in special_chars {
-        word_list.push(c);
-    }
-    word_list
+    char_list
 }
 
-fn init_population(population_size: i32, hard_mode: bool) -> Vec<Vec<char>> {
+fn init_population(population_size: u16, hard_mode: bool) -> Vec<Vec<char>> {
     let mut population: Vec<Vec<char>> = Vec::new();
     for _ in 0..population_size {
         if hard_mode {
@@ -66,7 +53,7 @@ fn init_population(population_size: i32, hard_mode: bool) -> Vec<Vec<char>> {
 }
 
 fn get_pop_score(pop: &Vec<char>, target: &String) -> i32 {
-    let mut score: i32 = 0;
+    let mut score: i32;
     if pop.len() == target.len() {
         score = 50;
     } else {
@@ -93,12 +80,17 @@ fn get_best_score(target: &String) -> i32 {
 
 fn make_child(parent1: &Vec<char>, parent2: &Vec<char>, mutation_rate: f32) -> Vec<char> {
     let mut rng = rand::thread_rng();
-    let crosspoint = rng.gen_range(0..(parent1.len()/2));
-    let vec1 = parent1[..crosspoint].to_vec();
-    let vec2 = parent2[crosspoint..].to_vec();
+    let crosspoint: usize;
+    if parent1.len() != parent2.len() {
+        crosspoint = rng.gen_range(0..(if parent1.len() < parent2.len() {parent1.len()} else {parent2.len()}));
+    } else {
+        crosspoint = rng.gen_range(0..(parent1.len()));
+
+    }
+    
     let mut child = vec![];
-    child.extend(vec1);
-    child.extend(vec2);
+    child.extend(parent1[..crosspoint].to_vec());
+    child.extend(parent2[crosspoint..].to_vec());
 
     for i in 0..child.len() {
         if rng.gen_range(0.0..1.0) < mutation_rate {
@@ -107,8 +99,7 @@ fn make_child(parent1: &Vec<char>, parent2: &Vec<char>, mutation_rate: f32) -> V
     }
 
     if rng.gen_range(0.0..1.0) < (mutation_rate / 10.0) {
-        let single_char: char = WORD_LIST[rng.gen_range(0..WORD_LIST.len())];
-        child.push(single_char);
+        child.push(WORD_LIST[rng.gen_range(0..WORD_LIST.len())]);
     }
     if rng.gen_range(0.0..1.0) < (mutation_rate / 10.0) {
         child.remove(0);
@@ -116,10 +107,10 @@ fn make_child(parent1: &Vec<char>, parent2: &Vec<char>, mutation_rate: f32) -> V
     child
 }
 
-fn generative_algo(target: &String, generations: i32, population_size: i32, mutation_rate: f32, hard_mode: bool) -> (Vec<char>, i32){
+fn generative_algo(target: &String, generations: u32, population_size: u16, mutation_rate: f32, hard_mode: bool) -> (Vec<char>, u32){
     let mut rng = rand::thread_rng();
     let mut population = init_population(population_size, hard_mode);
-    population.sort_by(|a, b| get_pop_score(&a.to_vec(), &target).cmp(&get_pop_score(&b.to_vec(), &target)).reverse());
+    population.sort_by(|a, b| get_pop_score(&b.to_vec(), &target).cmp(&get_pop_score(&a.to_vec(), &target)));
 
     let top_performer_range = population.len() / 5;
     let target_score = get_best_score(&target);
@@ -130,13 +121,11 @@ fn generative_algo(target: &String, generations: i32, population_size: i32, muta
 
         while next_generation.len() < population.len() {
             let mut iter = population.choose_multiple(&mut rng, 2);
-            let parent1 = iter.next().unwrap();
-            let parent2 = iter.next().unwrap();
 
-            next_generation.push(make_child(&parent1, &parent2, mutation_rate));
+            next_generation.push(make_child(&iter.next().unwrap(), &iter.next().unwrap(), mutation_rate));
         }
 
-        next_generation.sort_by(|a, b| get_pop_score(&a.to_vec(), &target).cmp(&get_pop_score(&b.to_vec(), &target)).reverse());
+        next_generation.sort_by(|a, b| get_pop_score(&b.to_vec(), &target).cmp(&get_pop_score(&a.to_vec(), &target)));
         population = next_generation;
 
         apex_pops = population[..top_performer_range].to_vec();
@@ -150,10 +139,9 @@ fn generative_algo(target: &String, generations: i32, population_size: i32, muta
     return (population.get(0).unwrap().to_vec(), generations);
 }
 
-
 fn main() {
     let target = generate_rand_word(10);
-    let generations = 10000;
+    let generations = 1000000;
     let population_size = 100;
     let mutation_rate = 0.025;
     let hard_mode = false;
@@ -165,10 +153,9 @@ fn main() {
     let elapsed_time = end_time - start_time;
 
 
-
+    println!("Len WORD_LIST: {}", WORD_LIST.len());
     println!("Target: {}", &target);
     println!("Result: {}", result.iter().collect::<String>());
     println!("After: {} generations", gens);
     println!("Elapsed time: {:?}", elapsed_time);
-
 }
